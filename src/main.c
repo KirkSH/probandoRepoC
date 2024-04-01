db0bbaf5b4e4529b940158c20b26716b1842c97a
Alexis Nubile <anubile@frba.utn.edu.ar>
2024-04-01 00:55:37 +0000

    creo foo() y goo()

--- src/main.c
+++ src/main.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <commons/txt.h>
 
+int foo(void) {
+    return 1;
+}
+
+int goo(void) {
+    return 1;
+}
+
 int main(int argc, char* argv[]) {
     txt_write_in_stdout("Hola, Operativos!!\n");
     return 0;
